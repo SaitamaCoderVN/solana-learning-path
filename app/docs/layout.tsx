2f62cdf9c4d817836259bab39c9d08d75213e6cb
lou1s <lou1svuong.dev@gmail.com>
2025-08-12 12:06:44 +0700

    Refactor layout component to enhance sidebar functionality and integrate dynamic tab icons using updated configuration options.

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,12 +1,30 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
+import { DocsLayout, DocsLayoutProps } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions, linkItems } from "@/app/layout.config";
+import { source } from "@/lib/source";
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [linkItems[linkItems.length - 1]],
+  sidebar: {
+    enabled: true,
+    tabs: {
+      transform(option, node) {
+        const meta = source.getNodeMeta(node);
+        if (!meta) return option;
+        return {
+          ...option,
+          icon: (
+            <div className="rounded-md shadow-lg [&_svg]:size-5">
+              {node.icon}
+            </div>
+          ),
+        };
+      },
+    },
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }
